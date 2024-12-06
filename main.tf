b12388fa01e1827e6e733869e4c0ce9691f79f8c
iustin05 <iusgrig@icloud.com>
2024-12-06 11:56:52 +0100

    feat: uptime kuma terraform config

--- main.tf
+++ main.tf
@@ -36,6 +36,11 @@ variable "subdomain" {
   default = "api"
 }
 
+variable "status_subdomain" {
+  description = "Subdomain for the status page"
+  default = "status"
+}
+
 provider "hcloud" {
   token = var.hcloud_token
 }
@@ -44,7 +49,7 @@ provider "cloudflare" {
   api_token = var.cloudflare_api_token
 }
 
-# Create a new server
+# Create PocketBase server
 resource "hcloud_server" "pocketbase" {
   name        = "pocketbase-server"
   image       = "ubuntu-22.04"
@@ -73,7 +78,31 @@ resource "hcloud_server" "pocketbase" {
               EOF
 }
 
-# Create A record in Cloudflare
+# Create Uptime Kuma server
+resource "hcloud_server" "uptime_kuma" {
+  name        = "uptime-kuma-server"
+  image       = "ubuntu-22.04"
+  server_type = "cax11"
+  location    = "nbg1"  # Nuremberg
+  
+  user_data = <<-EOF
+              #!/bin/bash
+              apt-get update
+              apt-get install -y docker.io
+              systemctl start docker
+              systemctl enable docker
+              
+              # Run Uptime Kuma container
+              docker run -d \
+                --name uptime-kuma \
+                --restart always \
+                -p 80:3001 \
+                -v uptime-kuma:/app/data \
+                louislam/uptime-kuma:1
+              EOF
+}
+
+# Create A record for PocketBase in Cloudflare
 resource "cloudflare_record" "api" {
   zone_id = var.cloudflare_zone_id
   name    = var.subdomain
@@ -82,11 +111,28 @@ resource "cloudflare_record" "api" {
   proxied = true # Enable Cloudflare proxy
 }
 
+# Create A record for Uptime Kuma in Cloudflare
+resource "cloudflare_record" "status" {
+  zone_id = var.cloudflare_zone_id
+  name    = var.status_subdomain
+  value   = hcloud_server.uptime_kuma.ipv4_address
+  type    = "A"
+  proxied = true # Enable Cloudflare proxy
+}
+
 # Output values
-output "server_ip" {
+output "pocketbase_ip" {
   value = hcloud_server.pocketbase.ipv4_address
+}
+
+output "uptime_kuma_ip" {
+  value = hcloud_server.uptime_kuma.ipv4_address
 }
 
 output "api_url" {
   value = "${var.subdomain}.${var.domain}"
+}
+
+output "status_url" {
+  value = "${var.status_subdomain}.${var.domain}"
 }
